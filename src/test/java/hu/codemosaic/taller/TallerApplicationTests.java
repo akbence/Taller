8f8fc104471d6f908c24287eebf6bd4aba68dbab
akben <akbence007@gmail.com>
2025-10-28 00:54:53 +0100

    Initial commit spring init

---
+++ src/test/java/hu/codemosaic/taller/TallerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.codemosaic.taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
