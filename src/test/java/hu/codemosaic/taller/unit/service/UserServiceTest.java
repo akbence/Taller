9fc5840d8d94b4beb4018707d6f057682a019943
akben <akbence007@gmail.com>
2025-11-01 00:23:35 +0100

    BaseApiController and UserServiceTest

--- src/test/java/hu/codemosaic/taller/unit/service/UserServiceTest.java
+++ src/test/java/hu/codemosaic/taller/unit/service/UserServiceTest.java
@@ -6,11 +6,7 @@ import hu.codemosaic.taller.repository.AppUserRepository;
 import hu.codemosaic.taller.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 import java.util.List;
