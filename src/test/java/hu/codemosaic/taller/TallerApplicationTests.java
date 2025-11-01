e8c347d1b35368553a7e4033a8d2984c56b1fd38
akben <akbence007@gmail.com>
2025-11-01 20:12:29 +0100

    fix test

--- src/test/java/hu/codemosaic/taller/TallerApplicationTests.java
+++ src/test/java/hu/codemosaic/taller/TallerApplicationTests.java
@@ -1,10 +1,15 @@
 package hu.codemosaic.taller;
 
+import hu.codemosaic.taller.security.JwtService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class TallerApplicationTests {
+
+	@MockitoBean
+	JwtService jwtService;
 
 	@Test
 	void contextLoads() {
